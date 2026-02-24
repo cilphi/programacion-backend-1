62e80d6ff0d556c16811774ab91592459fd01039
cilphi <silicioycilphi@gmail.com>
2026-02-24 14:37:12 -0300

    Correción de errores
    Se arregló la ruta para visualizar productDetail.handlebars

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,4 +1,5 @@
 import {Router} from 'express';
+import productsModel from '../models/products.model.js';
 
 const router = Router();
 
@@ -12,9 +13,17 @@ router.get('/products', (req,res)=>{
     res.render("products", {title: 'Nuestros productos'})
 })
 
-router.get('/products/:pid', (req,res)=>{
-    res.render("productDetail", {title: 'Detalle del producto'})
-})
+router.get('/products/:pid', async (req, res) => {
+    try {
+        const product = await productsModel.findById(req.params.pid).lean();
+        if (!product) {
+            return res.status(404).send("Producto no encontrado");
+        }
+        res.render("productDetail", { title: "Detalle del producto", product });
+    } catch (error) {
+        res.status(500).send("Error al cargar detalle");
+    }
+});
 
 router.get('/carts', (req,res)=>{
     res.render("carts", {title: 'Carro de compras'})
